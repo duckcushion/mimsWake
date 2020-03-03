0b31a2a661168b48f2f4c3d87a369c6b54cf4f4f
duckcushion <duckcushion7@gmail.com>
2020-03-03 12:23:48 +0900

    InboundFilePolling

--- src/main/java/com/mims/wake/server/inbound/InboundTcpSocketServer.java
+++ src/main/java/com/mims/wake/server/inbound/InboundTcpSocketServer.java
@@ -24,77 +24,86 @@ import io.netty.handler.logging.LoggingHandler;
  */
 public class InboundTcpSocketServer {
 
-    private static final Logger LOG = LoggerFactory.getLogger(InboundTcpSocketServer.class);
+	private static final Logger LOG = LoggerFactory.getLogger(InboundTcpSocketServer.class);
 
-    private final int port;		// Inbound Server listen port
+	private final int port; // Inbound Server listen port
 
-    private EventLoopGroup bossGroup;		// EventLoopGroup that accepts an incoming connection
-    private EventLoopGroup workerGroup;	// EventLoopGroup that handles the traffic of the accepted connection
+	private EventLoopGroup bossGroup; // EventLoopGroup that accepts an incoming connection
+	private EventLoopGroup workerGroup; // EventLoopGroup that handles the traffic of the accepted connection
+	private InboundFilePolling inboundFilePolling; // [YPK]
 
-    /**
-     * constructor with a paramter
-     * @param port Inbound Server listen port
-     */
-    public InboundTcpSocketServer(int port) {
-        this.port = port;
-    }
+	/**
+	 * constructor with a paramter
+	 * 
+	 * @param port Inbound Server listen port
+	 */
+	public InboundTcpSocketServer(int port) {
+		this.port = port;
+	}
 
-    /**
-     * InboundServer 인스턴스를 기동한다.<br>
-     * -TCP 스트림에 대한 메시지 구분자 지정<br>
-     * -소켓채널에 대한 이벤트 핸들러 지정<br>
-     * -소켓옵션 지정
-     * @param inboundQueues Inbound Queue collection
-     */
-    public void startup(Map<String, InboundQueue> inboundQueues) {
-        LOG.info("[InboundServer] starting...");
+	/**
+	 * InboundServer 인스턴스를 기동한다.<br>
+	 * -TCP 스트림에 대한 메시지 구분자 지정<br>
+	 * -소켓채널에 대한 이벤트 핸들러 지정<br>
+	 * -소켓옵션 지정
+	 * 
+	 * @param inboundQueues Inbound Queue collection
+	 */
+	public void startup(Map<String, InboundQueue> inboundQueues) {
+		LOG.info("[InboundServer] starting...");
 
-        bossGroup = new NioEventLoopGroup();
-        workerGroup = new NioEventLoopGroup();
-        try {
-            ServerBootstrap bootstrap = new ServerBootstrap();
-            bootstrap.group(bossGroup, workerGroup)
-                     .channel(NioServerSocketChannel.class)
-                     .handler(new LoggingHandler(LogLevel.INFO))
-                     .childHandler(new ChannelInitializer<SocketChannel>() {
-                         @Override
-                         public void initChannel(SocketChannel ch) {
-                             ChannelPipeline pipeline = ch.pipeline();
-                             //pipeline.addLast(new DelimiterBasedFrameDecoder(Integer.MAX_VALUE, PushConstant.DEFAULT_DELIMITER));
-                             //pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
-                             //pipeline.addLast(new PushMessageDecoder());
-                             //pipeline.addLast(new InboundTcpSocketServerHandler(inboundQueues));
-                             pipeline.addLast(new InboundTcpSocketServerMsgHandler(inboundQueues));
-                         }
-                     })
-                     .option(ChannelOption.SO_REUSEADDR, true)
-                     .childOption(ChannelOption.SO_KEEPALIVE, true)
-                     .childOption(ChannelOption.TCP_NODELAY, true);
+		bossGroup = new NioEventLoopGroup();
+		workerGroup = new NioEventLoopGroup();
+		try {
+			ServerBootstrap bootstrap = new ServerBootstrap();
+			bootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+					.handler(new LoggingHandler(LogLevel.INFO)).childHandler(new ChannelInitializer<SocketChannel>() {
+						@Override
+						public void initChannel(SocketChannel ch) {
+							ChannelPipeline pipeline = ch.pipeline();
+							// pipeline.addLast(new DelimiterBasedFrameDecoder(Integer.MAX_VALUE,
+							// PushConstant.DEFAULT_DELIMITER));
+							// pipeline.addLast(new StringDecoder(CharsetUtil.UTF_8));
+							// pipeline.addLast(new PushMessageDecoder());
+							// pipeline.addLast(new InboundTcpSocketServerHandler(inboundQueues));
+							pipeline.addLast(new InboundTcpSocketServerMsgHandler(inboundQueues));
+						}
+					}).option(ChannelOption.SO_REUSEADDR, true).childOption(ChannelOption.SO_KEEPALIVE, true)
+					.childOption(ChannelOption.TCP_NODELAY, true);
 
-            bootstrap.bind(port).sync();
+			bootstrap.bind(port).sync();
+			
+			// [+] [YPK] start InboundFilePlling
+			inboundFilePolling = new InboundFilePolling(500, "mimsWake");
+			inboundFilePolling.startup(inboundQueues);
+			// [-]
 
-            LOG.info("[InboundServer] started, listening on port " + port);
+			LOG.info("[InboundServer] started, listening on port " + port);
 
-        } catch (InterruptedException e) {
-            LOG.error("[InboundServer] failed to startup", e);
-            shutdown();
-        }
-    }
+		} catch (InterruptedException e) {
+			LOG.error("[InboundServer] failed to startup", e);
+			shutdown();
+		}
+	}
 
-    /**
-     * InboundServer 인스턴스를 중지한다.<br>
-     * -shutdown worker EventLoopGroup<br>
-     * -shutdown boss EventLoopGroup
-     */
-    public void shutdown() {
-        if (workerGroup != null) {
-            workerGroup.shutdownGracefully();
-        }
-        if (bossGroup != null) {
-            bossGroup.shutdownGracefully();
-        }
+	/**
+	 * InboundServer 인스턴스를 중지한다.<br>
+	 * -shutdown worker EventLoopGroup<br>
+	 * -shutdown boss EventLoopGroup
+	 */
+	public void shutdown() {
+		if (workerGroup != null) {
+			workerGroup.shutdownGracefully();
+		}
+		if (bossGroup != null) {
+			bossGroup.shutdownGracefully();
+		}
+		// [+] [YPK]
+		if (inboundFilePolling != null) {
+			inboundFilePolling.shutdown();
+		}
+		// [-]
 
-        LOG.info("[InboundServer] shutdown");
-    }
-
+		LOG.info("[InboundServer] shutdown");
+	}
 }
