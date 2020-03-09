cd4d1e6599d0e6badffa1bfec9dcf9b9d0c0cf94
duckcushion <duckcushion7@gmail.com>
2020-03-09 15:02:50 +0900

    NONE

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+﻿#!/bin/bash
 
 ### Create Package ###
 /Volumes/Data\ Storage/WORKSPACE/mimsWake/mvnw clean package
 
 #cd target
-java -jar target/websocket.war --spring.config.location=/config/application.yml --spring.profiles.active=production
+#java -jar target/websocket.war --spring.config.location=/config/application.yml --spring.profiles.active=production
+#java -jar target/websocket.war
